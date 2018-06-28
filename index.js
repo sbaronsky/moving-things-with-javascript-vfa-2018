3539532dba8e48f57233e9f957c58e2eaa1a11fc
Sara Baronsky <sbaronsk@bowdoin.edu>
2018-06-28 09:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
 
-  if (left < 0) {
-    dodger.style.left = `${left + 1}px`
+  if (right < 0) {
+    dodger.style.left = `${right - 1}px`
   }
 }
 
