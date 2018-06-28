6a0663dac62ccf6154efa36315db1b17fb9f63f0
Sara Baronsky <sbaronsk@bowdoin.edu>
2018-06-28 09:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ document.addEventListener('keydown', function(e) {
     dodger.style.left = `${left - 1}px`
   }
 })
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
