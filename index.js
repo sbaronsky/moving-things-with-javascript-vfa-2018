7f9348de81b5c94772c3662bc8017584bb474896
Sara Baronsky <sbaronsk@bowdoin.edu>
2018-06-28 09:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
